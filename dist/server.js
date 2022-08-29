76e62a78197b2a4f988196add92b44fdd3860a30
ArtuArtaza <aartazaesc@gmail.com>
2022-08-28 22:28:15 -0300

    populated orders

--- dist/server.js
+++ dist/server.js
@@ -5,6 +5,7 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 require("dotenv").config();
 const express_1 = __importDefault(require("express"));
+const cors_1 = __importDefault(require("cors"));
 const connectDB_1 = __importDefault(require("./utils/connectDB"));
 const auth_route_1 = __importDefault(require("./routes/auth.route"));
 const client_route_1 = __importDefault(require("./routes/client.route"));
@@ -17,7 +18,7 @@ const expense_route_1 = __importDefault(require("./routes/expense.route"));
 const server = (0, express_1.default)();
 const port = process.env.PORT || 5000;
 (0, connectDB_1.default)();
-server.use();
+server.use((0, cors_1.default)());
 server.use(express_1.default.json());
 server.use(express_1.default.urlencoded({ extended: false }));
 server.use("/auth", auth_route_1.default);
